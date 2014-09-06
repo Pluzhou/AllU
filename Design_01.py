0c05ec1c7fab63abe6d488e91d282fc5aa0b875b
tangys <393977464@qq.com>
2014-09-06 23:59:47 +0800

    Update Design01
    First design for Input and Output.

--- Design_01.py
+++ Design_01.py
@@ -1,8 +1,10 @@
 # coding:utf-8
 #
-# This is only one tmp file for Design
+# This is only one tmp file for Design 01 version
 
 __author__ = "tangys"
+
+__version__ = "0.1"
 
 """
 PreRequisites:
@@ -16,4 +18,33 @@ PreRequisites:
         2. wmi
         3. Django
         4. Numpy
+"""
+
+
+"""
+Input:
+
+    Packages:
+        1. wmi
+            get Win32_Process instances by wmi service.
+
+        2. time
+            record local time.
+
+        3. logging
+            script log.
+"""
+
+"""
+Output:
+
+    Packages:
+        1. Django
+            Use Django to display data.
+
+        2. Numpy
+            Analyisi the data captured from Input module.
+
+        3. logging
+            script log.
 """
