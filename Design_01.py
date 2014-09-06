1848494ef4ade666c056a319948f43f058bec211
tangys <393977464@qq.com>
2014-09-06 23:50:28 +0800

    Delete .txt and add Design_01.py (PreRequisites)
    Add some AllU PreRequisites

---
+++ Design_01.py
@@ -0,0 +1,19 @@
+# coding:utf-8
+#
+# This is only one tmp file for Design
+
+__author__ = "tangys"
+
+"""
+PreRequisites:
+
+    Environment for using:
+        1. Python 2.1 or later
+        2. Windows OS
+
+    3rd Packages need to install:
+        1. Pywin32
+        2. wmi
+        3. Django
+        4. Numpy
+"""
